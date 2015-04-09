11d7b0e7058a48619b927b35eda690cea2db9a8f
Janssen dos Reis Lima <janssenreislima@gmail.com>
2015-04-09 19:18:59 -0300

    Update reconhecer_evento_zabbix.py

--- reconhecer_evento_zabbix.py
+++ reconhecer_evento_zabbix.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Apr 09 18:37:18 2015
 
 @author: Janssen dos Reis Lima
+
+Uso: python reconhecer_evento_zabbix.py <event.id>
 """
 
 from zabbix_api import ZabbixAPI
