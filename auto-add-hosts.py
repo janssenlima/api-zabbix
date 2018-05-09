8457c4929f2bed03b5340bf9d970fb8918a7b727
Janssen dos Reis Lima <janssenreislima@gmail.com>
2018-05-09 17:43:19 -0300

    Migração para lib PyZabbix e algumas firulas
    Inclusão de barra de progresso para acompanhamento do cadastro dos hosts.

--- auto-add-hosts.py
+++ auto-add-hosts.py
@@ -1,38 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#author: Janssen dos Reis Lima - http://blog.conectsys.com.br
+#author: Janssen dos Reis Lima
 
-from zabbix_api import ZabbixAPI
+from pyzabbix import ZabbixAPI
 import csv
+from progressbar import ProgressBar, Percentage, ETA, ReverseBar, RotatingMarker, Timer
 
-zapi = ZabbixAPI(server="http://localhost/zabbix")
-zapi.login("Admin", "zabbix")
+zapi = ZabbixAPI("http://localhost/zabbix")
+zapi.login(user="Admin", password="zabbix")
+
+arq = csv.reader(open('/tmp/hosts.csv'))
+
+linhas = sum(1 for linha in arq)
 
 f = csv.reader(open('/tmp/hosts.csv'), delimiter=';')
+bar = ProgressBar(maxval=linhas,widgets=[Percentage(), ReverseBar(), ETA(), RotatingMarker(), Timer()]).start()
+i = 0
+
 for [hostname,ip] in f:
-    print "Cadastrando host da linha ", f.line_num
-    hostcriado = zapi.host.create({
-        "host": hostname,
-        "status": 1,
-        "interfaces": [
-            {
-                "type": 1,
-                "main": 1,
-                "useip": 1,
-                "ip": ip,
-                "dns": "",
-                "port": 10050
-            }
-        ],
-        "groups": [
-            {
-                "groupid": 2
-            }
-        ],
-        "templates": [
-            {
-                "templateid": 10001
-            }
-        ]
-    })
+    #print "Cadastrando host da linha ", f.line_num
+    hostcriado = zapi.host.create(
+        host= hostname,
+        status= 1,
+        interfaces=[{
+            "type": 1,
+            "main": "1",
+            "useip": 1,
+            "ip": ip,
+            "dns": "",
+            "port": 10050
+        }],
+        groups=[{
+            "groupid": 2
+        }],
+        templates=[{
+            "templateid": 10001
+        }]
+    )
+
+
+    i += 1
+    bar.update(i)
+
+bar.finish
+print " "
